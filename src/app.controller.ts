e051eb28a99a46e566973ff5ac70a51f6c85bdc2
miki <jcw2551@naver.com>
2025-05-28 15:58:59 +0900

    posts 모듈 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- */
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_offcial',
-      title: '뉴진스 민지',
-      content: '메이크업 고치는 민지',
-      likeCount: 10000000,
-      commentCount: 999999,
-    }
-  }
 }
+
+// nest g resource: 원하는 모듈을 생성할 수 있는 명령어
---
+++ src/posts/posts.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+
+describe('PostsController', () => {
+  let controller: PostsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PostsController],
+      providers: [PostsService],
+    }).compile();
+
+    controller = module.get<PostsController>(PostsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
